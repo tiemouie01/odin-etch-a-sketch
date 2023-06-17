d884e920c1db0c386a1107e93dcea1bb272faaca
Timothy Miamba <timal0361@gmail.com>
2023-06-17 19:44:27 +0200

    Add mouseover event listener to simulate a pen sketching

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ const container = document.querySelector('.container');
 
 for (let i = 1; i <= 16; i++){
     const rowDiv = document.createElement('div');
-    rowDiv.classList.toggle('row');
+    rowDiv.classList.add('row');
 
     for (let i = 1; i <= 16; i++){
         const columnDiv = document.createElement('div');
-        columnDiv.classList.toggle('column');
+        columnDiv.classList.add('column');
+        columnDiv.addEventListener('mouseover',() => columnDiv.classList.toggle('hover'));
         rowDiv.appendChild(columnDiv);
     }
     container.appendChild(rowDiv);
