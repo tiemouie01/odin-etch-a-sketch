ab4bd92d8522a5f22bf50282946837624fee020c
Timothy Miamba <timal0361@gmail.com>
2023-06-18 13:57:35 +0200

    Add button that enables a user to create a differently-sized grid

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 function createGrid(numSquares = 16){
     const container = document.querySelector('.container');
-
     for (let i = 1; i <= numSquares; i++){
         const rowDiv = document.createElement('div');
         rowDiv.classList.add('row');
 
-        for (let i = 1; i <= 16; i++){
+        for (let i = 1; i <= numSquares; i++){
             const columnDiv = document.createElement('div');
             columnDiv.classList.add('column');
             columnDiv.addEventListener('mouseover',() => columnDiv.classList.toggle('hover'));
@@ -15,7 +14,21 @@ function createGrid(numSquares = 16){
     }
 }
 
+function removeGrid(){
+    const rows = document.querySelectorAll('.row');
+    const columns = document.querySelectorAll('.column');
+
+    rows.forEach(row => row.remove());
+    columns.forEach(col => col.remove());
+}
+
+function newGrid(){
+    removeGrid();
+    const numSquares = prompt('Enter the number of squares for your new grid: ');
+    createGrid(numSquares);
+}
+
 createGrid();
 
 const button = document.querySelector('button');
-button.addEventListener('click',createGrid)
+button.addEventListener('click',newGrid)
