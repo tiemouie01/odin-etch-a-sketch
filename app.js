4b5e73e84c9c3a4c95517234942535ba1c54e99e
Timothy Miamba <timal0361@gmail.com>
2023-06-18 13:39:51 +0200

    Remove margin and padding from column divs

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
-const container = document.querySelector('.container');
+function createGrid(numSquares = 16){
+    const container = document.querySelector('.container');
 
-for (let i = 1; i <= 16; i++){
-    const rowDiv = document.createElement('div');
-    rowDiv.classList.add('row');
+    for (let i = 1; i <= numSquares; i++){
+        const rowDiv = document.createElement('div');
+        rowDiv.classList.add('row');
 
-    for (let i = 1; i <= 16; i++){
-        const columnDiv = document.createElement('div');
-        columnDiv.classList.add('column');
-        columnDiv.addEventListener('mouseover',() => columnDiv.classList.toggle('hover'));
-        rowDiv.appendChild(columnDiv);
+        for (let i = 1; i <= 16; i++){
+            const columnDiv = document.createElement('div');
+            columnDiv.classList.add('column');
+            columnDiv.addEventListener('mouseover',() => columnDiv.classList.toggle('hover'));
+            rowDiv.appendChild(columnDiv);
+        }
+        container.appendChild(rowDiv);
     }
-    container.appendChild(rowDiv);
 }
+
+createGrid();
+
+const button = document.querySelector('button');
+button.addEventListener('click',createGrid)
