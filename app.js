29d24880786247f0043bfa4738f81ef220922b86
Timothy Miamba <timal0361@gmail.com>
2023-06-19 19:36:40 +0200

    Add logic that prevents extreme grids from being requested

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-function createGrid(numSquares = 16){
+function createGrid(numSquares = 16) {
     const container = document.querySelector('.container');
     for (let i = 1; i <= numSquares; i++){
         const rowDiv = document.createElement('div');
@@ -14,7 +14,7 @@ function createGrid(numSquares = 16){
     }
 }
 
-function removeGrid(){
+function removeGrid() {
     const rows = document.querySelectorAll('.row');
     const columns = document.querySelectorAll('.column');
 
@@ -22,10 +22,36 @@ function removeGrid(){
     columns.forEach(col => col.remove());
 }
 
-function newGrid(){
+function getNumSqaures() {
+    let incorrect = true;
+    let numSquares;
+
+    while(incorrect) {
+        numSquares = prompt('Enter the number of squares per side for your new grid: ');
+
+        if (numSquares < 0 || numSquares > 100) {
+            alert('Only values between 0 and 100 are permitted for the new grid.');
+        } else {
+            incorrect = false;
+        }
+    }
+    return numSquares;
+}
+
+function newGrid() {
+
+    /* When the new grid button is clicked. The originial grid is removed.
+       
+       The number of squares per side is requested from the user. The 
+       program includes added logic that stops the user from entering
+       numbers-per-side values greater than 100.
+       
+       The new grid is then created.*/
+
     removeGrid();
-    const numSquares = prompt('Enter the number of squares for your new grid: ');
+    let numSquares = getNumSqaures();
     createGrid(numSquares);
+    alert(`A ${numSquares}x${numSquares} grid has been created.`);
 }
 
 createGrid();
